5cd9dca0c9f434d2c66fe56fffbfbe4f83e00771
Jaslan7 <103321034+Jaslan7@users.noreply.github.com>
2022-10-20 17:47:49 +0300

    jdksd (#4)

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,20 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div>
-            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, officia.</h1>
+        <div id="header">
+            <div className="container">
+                <div className="header">
+                    <h1>logo</h1>
+                    <div className="nav">
+                        <a href="#">Home</a>
+                        <a href="#">About</a>
+                        <a href="#">Cervices</a>
+                        <a href="#">Cervices</a>
+                        <a href="#">Cervices</a>
+                    </div>
+                    <button className="btn">login</button>
+                </div>
+            </div>
         </div>
     );
 };
