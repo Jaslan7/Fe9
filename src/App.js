3ff34a914c3f2092fcd26b6d408c23cc58552613
bakytdev <azimbakyt111@gmail.com>
2022-10-20 20:07:06 +0600

    new componets footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from "./components/Header/Header";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <Footer/>
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer id="footer">
+            <div className="container">
+                <div className="footer">
+                    <h1>
+                        hello footer
+                    </h1>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
