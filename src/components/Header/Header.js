33bcc00a3a8a61b31b45448871a1b6fe8ca34d7f
Jaslan7 <jasisaparbekov@gmail.com>
2022-10-20 16:56:36 +0300

    initial

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, officia.</h1>
+        </div>
+    );
+};
+
+export default Header;
