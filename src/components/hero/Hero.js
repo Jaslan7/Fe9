56de32cc2d51a93e23337ecbe4395de2f252561c
Maratbekov <asanovkurmanbek93@gmail.com>
2022-10-20 20:09:52 +0600

    Changed hero component

---
+++ src/components/hero/Hero.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div id="hero">
+            <div className="container">
+                <div className="hero">
+                    <h1>hero block</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;
